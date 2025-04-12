81a274964ebb6085a5c06af41ab01599e3e1df9c
davidrt301 <davidrt321443@gmail.com>
2025-04-12 11:17:38 -0500

    feature: se crea interaccion listar metas ,crear metas, actualizar metas, borrar metas y se crea back simulado

--- src/app/core/app-component/app.component.ts
+++ src/app/core/app-component/app.component.ts
@@ -13,3 +13,9 @@ import { PieComponent } from '../../layout/pie/pie.component';
 export class AppComponent {
   title = 'app-metas';
 }
+/* comndos git
+git add .
+ commit -m "feature: se crea interaccion listar metas ,crear metas, 
+ actualizar metas, borrar metas y se crea back simulado"
+ git push origin main 
+ */
